fdd8b8f84576b202630852ecd08638eb6531d534
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-14 16:39:50 -0300

    Parte final - Tratamento de erros

--- exemplos/tratamento-de-erros.ts
+++ exemplos/tratamento-de-erros.ts
@@ -1,24 +1,31 @@
-function nome(texto: any){
-    texto.split('/')
-}
+// function nome(texto: any){
+//     texto.split('/')
+// }
+// try{
+//     //tentar fazer tudo o que está aqui
+//     console.log('Adriano');
+//     nome(123)
+// } catch(erro){
+//     //caso algum erro aconteça, ele é capturado aqui
+//     if(erro instanceof TypeError){
+//         console.log(erro.message)
+//     }
+//     // console.log('Erro desconhecido'); 
+// } 
+// finally{
+//     //independente do erro ou não, esse bloco de código será executado
+//     console.log('Sempre será executado');
+// }
 
-try{
-    //tentar fazer tudo o que está aqui
-    console.log('Adriano');
-    
-    nome(123)
-} catch(erro){
-    //caso algum erro aconteça, ele é capturado aqui
-    if(erro instanceof TypeError){
-        console.log(erro.message)
-    }
-    // console.log('Erro desconhecido');
-    
-} 
+try {
+    console.log('Antes de lançar um erro')
 
+    //lançar um erro
+    // throw 'Um erro criado por mim'
+    throw new Error('Um erro do javascript')
 
-finally{
-    //independente do erro ou não, esse bloco de código será executado
-    console.log('Sempre será executado');
-    
+} catch (erro) {
+   if (erro instanceof Error) {
+    console.log(erro.message)
+   }
 }
